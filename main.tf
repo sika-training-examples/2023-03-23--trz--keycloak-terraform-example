4ec0d490c188822911dfe5d7edd914e7a0228c5c
Ondrej Sika <ondrej@ondrejsika.com>
2023-03-23 09:18:38 +0100

    feat: Create realm example

---
+++ main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    keycloak = {
+      source = "mrparkers/keycloak"
+    }
+  }
+}
+
+provider "keycloak" {
+  client_id = "admin-cli"
+  url       = "http://localhost:8080"
+  username  = "admin"
+  password  = "admin"
+}
+
+resource "keycloak_realm" "example" {
+  realm                  = "example"
+  enabled                = true
+  display_name           = "SSO"
+  display_name_html      = "<h1 style=\"color: red; font-size: 2em\">SSO</h1>"
+  reset_password_allowed = true
+  remember_me            = true
+  smtp_server {
+    host                  = "mailhog"
+    port                  = "1025"
+    from                  = "sso@example.com"
+    from_display_name     = "Example SSO"
+    reply_to              = "support@example.com"
+    reply_to_display_name = "Example Support"
+    auth {
+      username = "xxx"
+      password = "xxx"
+    }
+  }
+}
